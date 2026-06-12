a50fd6b93140e6b1c454fdcdb772d88d18ae2ae5
AvsheshSavani7 <138287046+AvsheshSavani7@users.noreply.github.com>
2026-06-12 18:31:27 +0530

    Add new email report type for other newswire in config.py

--- backend/config.py
+++ backend/config.py
@@ -55,6 +55,7 @@ EMAIL_REPORT_TYPES = {
     "newswire_new_deal_without_threshold": "Newswire New Deal Without Threshold",
     "newswire_new_deal_with_threshold": "Newswire New Deal With Threshold",
     "newswire_rss_match": "Newswire RSS Match Deal",
+    "other_newswire": "Other Newswire",
     "foreign_regulatory_us_deal": "Foreign Regulatory US Deal",
     "foreign_regulatory_matched_deal": "Foreign Regulatory Matched Deal"
 }
